011ba66ef4e253a30ba5a338ac737216a205ff4f
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-03 16:29:14 +0200

    Declare method that print board to stdout

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -13,6 +13,11 @@ public class TicTacToe {
     final private Player playerTwo;
 
     /**
+     *
+     */
+    final private String[][] board;
+
+    /**
      * Creates an instance of TicTacToe.
      * @param playerOne - Player one.
      * @param playerTwo - Player two.
@@ -20,6 +25,12 @@ public class TicTacToe {
     public TicTacToe(Player playerOne, Player playerTwo) {
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
+
+        // Initialize a matrices.
+        this.board = new String[][] {
+            {"1", "2", "3"},
+            {"4", "5", "6"},
+            {"7", "8", "9"}};
     }
 
     /**
@@ -28,5 +39,19 @@ public class TicTacToe {
     public void assignSymbol() {
         this.playerOne.setSymbol("X");
         this.playerTwo.setSymbol("O");
+    }
+
+    /**
+     * Prints the TicTacToe board to the standard output.
+     */
+    public void printBoard() {
+        System.out.println("|---|---|");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " +this.board[i][j]);
+            }
+            System.out.println(" |");
+        }
     }
 }
