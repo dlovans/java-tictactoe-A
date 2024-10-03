17e9381c6fdb7c46c4928757b9ac65facf76450c
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-03 13:42:30 +0200

    Declare TicTacToe class

---
+++ src/TicTacToe.java
@@ -0,0 +1,26 @@
+/**
+ * Represents Tic-Tac-Toe game.
+ */
+public class TicTacToe {
+    /**
+     * Player one.
+     */
+    final private Player playerOne;
+
+    /**
+     * Player two.
+     */
+    final private Player playerTwo;
+
+    /**
+     * Creates an instance of TicTacToe.
+     * @param playerOne - Player one.
+     * @param playerTwo - Player two.
+     */
+    public TicTacToe(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
+
+}
